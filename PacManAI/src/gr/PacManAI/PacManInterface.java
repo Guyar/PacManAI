1c129901b1b4bb9ce9eb27f42004664e293aaf0c
Guy Richardson <guyarich@hotmail.com>
2017-02-10 03:23:13 +0000

    Added while loop to PacManInterface that tells pacman to move up

--- PacManAI/src/gr/PacManAI/PacManInterface.java
+++ PacManAI/src/gr/PacManAI/PacManInterface.java
@@ -4,11 +4,15 @@ import java.awt.Robot;
 
 public class PacManInterface {
 	Robot robot;
+	static int delay = 10;
 	
 	public static void main(String[] args) throws Exception {
 		PacManInterface pac = new PacManInterface();
 		PacController controller = new PacController();
-		
+		while(true) {
+			controller.move(1);
+			Thread.sleep(delay);
+		}
 	}
 	
 	public PacManInterface() throws Exception {
